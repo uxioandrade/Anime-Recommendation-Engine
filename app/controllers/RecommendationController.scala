ca49c719a5aa5e247416a54c93d08b6c64e8b9dd
Uxío García Andrade <uxiog21@gmail.com>
2019-09-01 21:38:44 +0200

    change recommendation controller to singleton

---
+++ app/controllers/RecommendationController.scala
@@ -0,0 +1,58 @@
+package controllers
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import util.Database
+import util.ReactiveDB
+import play.api.libs.json.Json
+import play.api.libs.json.JsValue
+import util.Recommender
+import javax.inject._
+
+
+@Singleton
+class RecommendationController @Inject()(recommender: Recommender)
+extends Controller {
+
+  def topRated() = Action.async {
+    val itemsF = ReactiveDB.topRatedProducts.collect[Seq](100, ReactiveDB.handleProductsError())
+    itemsF map { items =>
+      Ok(views.html.top_rated_products(items))
+    }
+  }
+
+  def mostPopular() = Action.async {
+    val itemsF = ReactiveDB.mostPopularProducts.collect[Seq](100, ReactiveDB.handleProductsError())
+    itemsF map { items =>
+      Ok(views.html.most_popular_products(items))
+    }
+  }
+
+  def trends() = Action {
+    Ok(views.html.trending())
+  }
+
+  def trendingProduct(year: Int, month: Int) = Action.async {
+    val tpF = recommender.findTrendingProductsFor(year, month - 1)
+    tpF.map { items =>
+      Ok(Json.toJson(items))
+    }
+  }
+
+  def trendingCustomer(year: Int, month: Int) = Action.async {
+    val tpF = recommender.findTrendingCustomersFor(year, month - 1)
+    tpF.map { items =>
+      Ok(Json.toJson(items))
+    }
+  }
+
+  def recommendationsForCustomer(customerId: String) = Action.async {
+    val tpF = recommender.findForCustomer(customerId)
+    tpF.map { items =>
+      Ok(Json.toJson(items))
+    }
+  }
+
+}
