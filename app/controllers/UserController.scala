04b589c134ecf303a9727658533e91e9ab54eaa0
Uxío García Andrade <uxiog21@gmail.com>
2019-10-06 15:05:24 +0200

    init usercontroller

---
+++ app/controllers/UserController.scala
@@ -0,0 +1,26 @@
+package controllers
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import play.api.mvc._
+
+import play.Logger
+import javax.inject._
+import play.api.libs.json._
+
+import model.UserService
+
+@Singleton
+class UserController @Inject()(
+    userService: UserService,
+    recommender: Recommender)
+extends Controller {
+
+
+def getUser(user: String) = Action {
+      println("CHECKPOINT 1")
+      userService.getOneUser()
+      Ok
+  }
+
+}
