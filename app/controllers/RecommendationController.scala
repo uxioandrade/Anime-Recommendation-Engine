3797b2dee01c8ba91b9f93661a1debfddead9796
Uxío García Andrade <uxiog21@gmail.com>
2019-10-06 15:04:50 +0200

    add recommendation controller

--- app/controllers/RecommendationController.scala
+++ app/controllers/RecommendationController.scala
@@ -6,14 +6,70 @@ import play.api.mvc.Action
 import play.api.mvc.Controller
 import util.Database
 import util.ReactiveDB
+import model.UserService
+import model.RecommendationService
+import model.AnimeService
+import generated.enums.Animetype
+import util.SparkAlsUtility
+import org.apache.spark.mllib.recommendation.Rating
 import play.api.libs.json._
-//import util.Recommender
+import play.api.data.Forms._
+import play.api.data._
+import play.api.mvc._
 import javax.inject._
 
 
 @Singleton
-class RecommendationController  @Inject()(recommender: Recommender)
+class RecommendationController  @Inject()(
+  recommendendationService: RecommendationService,
+  animeService: AnimeService,
+  usersService: UserService
+  )
 extends Controller {
+
+  def userRecommendationForm(): Form[String] = 
+    Form (
+      "username" -> nonEmptyText
+    )
+
+  def formatRecommendationAsJson(recommendations: Map[Long,Double],animeData: Map[Long,(String, String)]): JsArray = {
+    var animeArray = new JsArray
+    animeData map { 
+      case (k,(v1,v2)) => {
+        animeArray = animeArray :+ Json.obj(
+          "id" -> k,
+          "title" -> v1,
+          "imageUrl" -> v2,
+          "score" -> recommendations(k)
+        )
+      }
+    }
+    animeArray
+  }
+  
+  def getRecommendationForUser(username: String) = Action { implicit req =>
+    userRecommendationForm().bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest("Error when retrieving this user's recommendations")
+      },
+        form => {
+          val user = "karthiga"
+          //val userId = usersService.getUserIdByUsername(user)
+          val recommendations = SparkAlsUtility.makePrediction(0)
+          val recommendationsIds = recommendations.map(x => x.product.toLong).toList
+          val recommendationsMap = recommendations.map(
+            x =>
+              x.product.toLong -> x.rating
+          ).toMap
+          Ok(
+            formatRecommendationAsJson(
+              recommendationsMap,
+              animeService.getSeqOfAnimesWithTheirId(recommendationsIds)
+            )
+          )
+      }
+    )
+  }
 /*
   def topRated() = Action.async {
     val itemsF = ReactiveDB.topRatedProducts.collect[Seq](100, ReactiveDB.handleProductsError())
