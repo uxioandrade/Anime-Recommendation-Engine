770400305c95bae320b3786ea88be38a9a32f50f
Uxío García Andrade <uxiog21@gmail.com>
2020-01-14 22:10:45 +0100

    update tests

---
+++ test/RecommendationControllerSpec.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import org.scalatest.plus
+import org.scalatestplus.play.guice._
+import play.api.test._
+import play.api.test.Helpers._
+
+class RecommendationControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+
+    
+
+
+}
